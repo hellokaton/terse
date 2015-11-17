9d1637b945d56ffb2eba6b4b51869e374b71ba91
biezhi <biezhi.me@gmail.com>
2015-11-17 17:51:38 +0800

    -

--- header.php
+++ header.php
@@ -4,14 +4,13 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<title><?php $this->archiveTitle(array('category' => _t('分类 %s 下的文章'), 'search' => _t('包含关键字 %s 的文章'), 'tag' => _t('标签 %s 下的文章'), 'author' => _t('%s 发布的文章')), '', ' - '); ?><?php $this->options->title(); ?></title>
+	<meta name="description" content="<?php $this->description() ?>" />
+	<meta name="keywords" content="<?php $this->keywords() ?>" />
 	<?php if ($this->options->siteIcon): ?>
     <link rel="Shortcut Icon" href="<?php $this->options->siteIcon() ?>" />
     <link rel="Bootmark" href="<?php $this->options->siteIcon() ?>" />
-	<?php endif; ?>
-	
+	<?php endif; ?>	
 	<link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
-    	<?php $this->header('generator=&template=&pingback=&xmlrpc=&wlw=&alternate='); ?>
-
     <script type="text/javascript" src="//cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
 </head>
 <body>
