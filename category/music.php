8800ab105578850a8e86d6eb2c6298e8b2966023
biezhi <biezhi.me@gmail.com>
2015-11-30 15:11:26 +0800

    add music category

--- category/music.php
+++ category/music.php
@@ -3,15 +3,23 @@ $this->need('header.php'); ?>
 <div class="content">
 	<div class="container">
 		<?php while($this->next()): ?>
-			<div class="entry music-player">
+			<div class="entry">
 				<div class="entry-left">
 					<h1 class="entry-title">
 						<a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
 					</h1>
+					<div class="entry-meta" itemprop="keywords">
+						<span>by <a href="<?php $this->author->permalink(); ?>"><?php $this->author() ?></a> · 
+						<?php $this->date('Y-m-d');?></span> · <?php $this->category(','); ?>
+					</div>
 				</div>
-				<div>			
+				<div class="entry-content">			
 					<?php $this->content(); ?>
 				</div>
+				<div class="post-tags music-tags">
+					<?php $this->tags(' ', true, ''); ?>
+				</div>
+				<div class="clearfix"></div>
 			</div>
 		<?php endwhile; ?>
 		<div class="pagination" role="pagination">
