c27827c0b3f0643b7d3d1c0aaeeabaf74cf0033c
biezhi <biezhi.me@gmail.com>
2015-12-01 18:07:31 +0800

    兼容移动端

--- js/main.js
+++ js/main.js
@@ -3,6 +3,34 @@ $('.loading').animate({'width':'100%'},50);
 $(document).ready(function(){
 	 $('.loading').fadeOut();
 
+  // 标题带动画下划线
+   if($(window).width() >= 768){
+      $('.entry-title a').each(function() {
+            var th = $(this), brd = $('<div></div>').appendTo(th).css({ position: 'absolute', borderTop: '2px solid #333',
+                      left: 2, top: 37, height: 0, width: 0});
+            th.on('mouseenter', function() {
+                $.when(brd)
+                    .then(function() {
+                    brd.css({
+                        left: 2,
+                        right: 'auto'
+                    }).animate({
+                        width: th.width()
+                    })
+                })
+            }).on('mouseleave', function() {
+                $.when(brd).then(function() {
+                    brd.stop().css({ right: 0, left: 'auto'
+                    }).animate({width: 0
+                    }, {complete: function() {
+                            brd.css({ left: 2, right: 'auto' })
+                        }
+                    })
+                })
+            });
+      });
+   }
+
    var $backToTopEle = $('<div class="backToTop" title="回到顶部"></div>').appendTo($("body")).click(function() {
         $("html, body").animate({ scrollTop: 0 }, 550);
    }), $backToTopFun = function() {
@@ -19,30 +47,5 @@ $(document).ready(function(){
    $(function() {
         $backToTopFun(); 
    });
-
-   // 标题带动画下划线
-   $('.entry-title a').each(function() {
-      var th = $(this), brd = $('<div></div>').appendTo(th).css({ position: 'absolute', borderTop: '2px solid #333',
-                left: 2, top: 37, height: 0, width: 0});
-        th.on('mouseenter', function() {
-            $.when(brd)
-                .then(function() {
-                brd.css({
-                    left: 2,
-                    right: 'auto'
-                }).animate({
-                    width: th.width()
-                })
-            })
-        }).on('mouseleave', function() {
-            $.when(brd).then(function() {
-                brd.stop().css({ right: 0, left: 'auto'
-                }).animate({width: 0
-                }, {complete: function() {
-                        brd.css({ left: 2, right: 'auto' })
-                    }
-                })
-            })
-        });
-    })
+   
 });
