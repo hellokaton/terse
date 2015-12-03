a1ab6fd91fe9f339a95b48e80f19c69777b0a1e0
biezhi <biezhi.me@gmail.com>
2015-12-03 10:46:55 +0800

    添加可配置多说域名

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
+/*
+ * 主题配置
+ */
 function themeConfig($form) {
     $siteIcon = new Typecho_Widget_Helper_Form_Element_Text('siteIcon', NULL, _t('http://demo.sc.chinaz.com/Files/pic/icons/5712/5.png'), _t('网站Icon'), _t('在这里填入一个图片URL地址, 作为网站Icon, 默认不显示'));
     $form->addInput($siteIcon);
@@ -9,10 +12,39 @@ function themeConfig($form) {
 
     $cnzzCode = new Typecho_Widget_Helper_Form_Element_Text('cnzzCode', NULL, _t(''), _t('统计代码'), _t('输入统计代码，没有则留空'));
     $form->addInput($cnzzCode);
+    
+    $duoshuoName = new Typecho_Widget_Helper_Form_Element_Text('duoshuoName', NULL, _t('blog-biezhi'), _t('多说评论'), _t('输入你的多说短名称'));
+    $form->addInput($duoshuoName);
 	
     $extPlugin = new Typecho_Widget_Helper_Form_Element_Checkbox('extPlugin', array(
         'UseFancybox' => _t('启用FancyBox')),
         // 'UseInstantclick' => _t('启用instantclick')
     array('UseFancybox'), _t('扩展项'));
     $form->addInput($extPlugin->multiMode());
+}
+
+/*
+* 手机移动设备识别函数
+*/
+function is_mobile() {
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $mobile_browser = Array(
+        "mqqbrowser", //手机QQ浏览器
+        "opera mobi", //手机opera
+        "juc","iuc",//uc浏览器
+        "fennec","ios","applewebKit/420","applewebkit/525","applewebkit/532","ipad","iphone","ipaq","ipod",
+        "iemobile", "windows ce",//windows phone
+        "240x320","480x640","acer","android","anywhereyougo.com","asus","audio","blackberry",
+        "blazer","coolpad" ,"dopod", "etouch", "hitachi","htc","huawei", "jbrowser", "lenovo",
+        "lg","lg-","lge-","lge", "mobi","moto","nokia","phone","samsung","sony",
+        "symbian","tablet","tianyu","wap","xda","xde","zte"
+    );
+    $is_mobile = false;
+    foreach ($mobile_browser as $device) {
+        if (stristr($user_agent, $device)) {
+            $is_mobile = true;
+            break;
+        }
+    }
+    return $is_mobile;
 }
